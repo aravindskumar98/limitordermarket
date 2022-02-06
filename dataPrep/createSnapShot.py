f9782df4eb6358b5fbbec98e335ec73294603406
Aravind <aravindskumar1998@gmail.com>
2022-02-06 03:56:06 -0500

    Update createSnapShot.py

--- dataPrep/createSnapShot.py
+++ dataPrep/createSnapShot.py
@@ -8,25 +8,31 @@ Created on Sun Feb  6 03:02:37 2022
 import pandas as pd
 import numpy as np
 
-
+## class for creating snapshots
 class snapshot:
     
     def __init__(self,filepath,intervalSize):
         # id,time,vd,vo,lp,isbuy
+   
+        ## Load data onto a numpy array
         self.data = self.getdata(filepath)
+        ## store the interval size/step size --> can be changed for finer control
         self.intervalSize = intervalSize 
+        
+        ## initialising the descriptors of the state
         self.sell, self.buy = [],[]
         self.sellSize,self.buySize = 0,0
         self.BASpread, self.volatility = 0,0
         
-    def getdata(self,filepath):
-        df = np.loadtxt('numpydatabook.txt',dtype = np.float32)
+    def getdata(self,filepath): ## function to load data from the filepath
+        df = np.loadtxt(filepath',dtype = np.float32)
         return df
         
+    ## This function resets the state and sets it back to the initial empty condition
     def resetState(self):
         self.sell, self.buy = [],[]
         self.sellSize, self.buySize = 0,0
-        print("Creating a new Snap of the limit-book..")
+        print("Initialising a new Snap of the limit-book..")
     
     def sortHeap(self):
         ## heap will be implemented in later updates
@@ -35,9 +41,10 @@ class snapshot:
         
     
     def generateSnap(self,startTime):
-        
+        ## reset leftovers from the last state
         self.resetState()
         
+        ## Iterate through all transactions within the intervalSize
         for row in self.data:
             if row[1] < startTime+self.intervalSize:
                 if row[-1]==1:
@@ -46,9 +53,11 @@ class snapshot:
                 else:
                     self.sell.append(row)
                     self.sellSize+=1
-        
+                        
+        ## sort the buy side and sell side postings and generate current snapshot
         self.sortHeap()
-        
+    
+    ## function to calculate descriptors                   
     ##function to display stuff
                     
         
