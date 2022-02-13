166413324b01810ce8cce9020f5784501ddd9074
aravindskumar1998 <akumar81@jh.edu>
2022-02-13 15:56:34 -0500

    added daily reset to the environment

--- dataPrep/createSnapShot.py
+++ dataPrep/createSnapShot.py
@@ -8,29 +8,43 @@ Created on Sun Feb  6 03:02:37 2022
 import pandas as pd
 import numpy as np
 
-## class for creating snapshots
-class orderbook:
+class env():
+    
     def __init__(self,filepath,intervalSize):
-        # id,time,vd,vo,lp,isbuy
+        self.data_array = None
+        self.load_data(filepath) ## Creates array of numpy array based on date
+        self.orderbook_daily = []
+        self.intervalSize = intervalSize * 60 ## in seconds
+        for data in self.data_array:
+            self.orderbook_daily.append(Orderbook(data,self.intervalSize))
+
+    def load_data(self,filepath):
+        df = np.loadtxt(filepath,dtype = np.float32)
+        self.data_array = np.split(df, np.where(np.diff(df[:,1]))[0]+1)
+
+## class for creating snapshots
+class Orderbook:
+    def __init__(self,data,intervalSize):
+        # id,date,time,vd,vo,lp,isbuy
    
         ## Load data onto a numpy array
-        self.data = self.getdata(filepath)
+        self.data = data
+        print('----------------------------------------------')
         ## store the interval size/step size --> can be changed for finer control
-        self.intervalSize = intervalSize * 60
-        self.tstart = self.data[0][1]
-        self.tend = self.data[-1][1]
+        self.intervalSize = intervalSize
+        self.tstart = self.data[0][2]
+        self.tend = self.data[-1][2]
         
         self.snaps = []
         self.generate_snaps()        
     
-    def getdata(self,filepath): ## function to load data from the filepath
-        df = np.loadtxt(filepath,dtype = np.float32)
-        return df
+
     
     def generate_snaps(self):
         start = self.tstart
         while start+self.intervalSize < self.tend:
             snap = snapshot(self.data,start,self.intervalSize)
+            snap.describe()
             self.snaps.append(snap)
             start+=self.intervalSize       
 
@@ -84,12 +98,12 @@ class snapshot:
         
         ## Iterate through all transactions within the intervalSize
         for row in self.data:
-            if row[1] < self.startTime+self.intervalSize:
+            if row[2] < self.startTime+self.intervalSize:
                 if row[-1]==1:
-                    self.buy.append(row[1])
+                    self.buy.append(row[2])
                     self.buySize+=1
                 else:
-                    self.sell.append(row[1])
+                    self.sell.append(row[2])
                     self.sellSize+=1
                 
                 ## sort the buy side and sell side postings and generate current snapshot
@@ -110,14 +124,15 @@ class snapshot:
     
     def calculate_state(self):
         if self.buy and self.sell:
-            self.BASpread = max(self.sell[-1]-self.buy[-1],0)
+            self.BASpread = self.sell[-1]-self.buy[-1]
         self.find_volatility()
 
     ##function to display stuff
     def describe(self):
         print("SELL SIDE")
+        size = min(len(self.sell),6)
         for i in range(1,min(len(self.sell),6)):
-            print(self.sell[-6+i])
+            print(self.sell[-size+i])
         
         print("<---------->")
         for i in range(1,min(len(self.buy),6)):
@@ -130,8 +145,8 @@ class snapshot:
         print(f"Current bid ask spread = {self.BASpread}")
             
 
-Order = orderbook("../data/numpydatabook.txt",30)
-Order.snaps[0].describe()
+Env = env("../data/numpydatabook1.txt",30)
+Env.orderbook_daily[3].snaps
         
                     
         
